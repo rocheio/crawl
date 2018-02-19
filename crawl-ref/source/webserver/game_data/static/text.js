139edaa2d5082a4e1d936a331663ac96999e0ebc
Aidan Holm <aidanholm@gmail.com>
2018-02-19 22:38:44 +0800

    Add webtiles popup UI stack
    This commit adds an additional webtiles client API to show popups.
    Unlike the existing show_dialog() and hide_dialog() functions, the new
    API supports a stack of popups.
    
    This commit converts all client use of the old API to the newer API;
    some places still call the older APIs, however, in order to behave
    correctly if an older version of the game is being loaded.

--- crawl-ref/source/webserver/game_data/static/text.js
+++ crawl-ref/source/webserver/game_data/static/text.js
@@ -4,17 +4,22 @@ define(["jquery", "comm"], function ($, comm) {
     var line_span = $("<span>");
     line_span.css("white-space", "pre");
 
+    function get_container(id)
+    {
+        return $("#ui-stack").children().last().find("." + id);
+    }
+
     function get_text_area_line(name, line)
     {
-        var area = $("#" + name);
-        var lines = $("#" + name + " > span");
+        var area = get_container(name)
+        var lines = area.children("span");
         for (var i = lines.length; i <= line; ++i)
         {
             if (i != 0)
                 area.append("<br>");
             area.append(line_span.clone());
         }
-        return $("#" + name + " > span").eq(line);
+        return area.children("span").eq(line);
     }
 
     function set_text_area_line(name, line, content)
@@ -25,9 +30,10 @@ define(["jquery", "comm"], function ($, comm) {
 
     function handle_text_update(data)
     {
+        var area = get_container(data.id)
         if (data.clear)
         {
-            var lines = $("#" + data.id + " > span");
+            var lines = area.children("span");
             for (var i = 0; i < lines.length; ++i)
             {
                 if (!(i in data.lines))
@@ -36,7 +42,7 @@ define(["jquery", "comm"], function ($, comm) {
         }
         for (var line in data.lines)
             set_text_area_line(data.id, line, data.lines[line]);
-        $("#" + data.id).trigger("text_update");
+        area.trigger("text_update");
     }
 
     comm.register_handlers({
